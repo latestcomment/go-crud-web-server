5ee1fa158e87e72bf0479124d6ff7581ee53c8be
exa.heydemans <exa.heydemans@doku.com>
2025-07-26 18:14:32 +0700

    dev first ini

--- main.go
+++ main.go
@@ -1,19 +1,22 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-
 	"crud-web-server/db"
 	"crud-web-server/handlers"
+	"fmt"
+	"net/http"
 )
 
 func main() {
 	db.InitDB()
 
-	http.HandleFunc("/user/insert", handlers.InsertUserHandler)
-	http.HandleFunc("/user/update", handlers.UpdateUserHandler)
+	// http.HandleFunc("/user/insert", handlers.InsertUserHandler)
+	// http.HandleFunc("/user/update", handlers.UpdateUserHandler)
 
+	http.HandleFunc("/customers", handlers.GetAllCustomersHandler)
+	http.HandleFunc("/customer", handlers.GetCustomerHanlder)
+	http.HandleFunc("/customer/update", handlers.UpdateCustomerHandler)
+	http.HandleFunc("/customer/delete", handlers.DeleteCustomerHandler)
 	fmt.Println("Server running on http://localhost:8001")
 	http.ListenAndServe(":8001", nil)
 }
--- models/user.go
+++
@@ -1,8 +0,0 @@
-package models
-
-import "github.com/google/uuid"
-
-type User struct {
-	ID   uuid.UUID `json:id`
-	Name string    `json:name`
-}
